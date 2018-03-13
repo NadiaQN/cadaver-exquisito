45c7d242e31ce23f73722f6443189164be4f61ce
NadiaQN <quezada.nadia@gmail.com>
2018-03-13 15:45:23 -0300

    tercera clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
-
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { PoemComponent } from './poem/poem.component';
 import { VersoFormComponent } from './verso-form/verso-form.component';
 import { VersoComponent } from './verso/verso.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { RegisterComponent } from './register/register.component';
+import { RegisterModalComponent } from './register-modal/register-modal.component';
+import { AuthService } from './auth.service';
+// Material Angular
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 
 
 @NgModule({
@@ -14,14 +23,25 @@ import { VersoComponent } from './verso/verso.component';
     AppComponent,
     PoemComponent,
     VersoFormComponent,
-    VersoComponent
+    VersoComponent,
+    RegisterComponent,
+    RegisterModalComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     ReactiveFormsModule,
-    BrowserModule
-
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    RegisterComponent,
+    RegisterModalComponent
+  ]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC-KNB0ewrW0y2BndL3i3zfHGF44p9z9oA",
+    authDomain: "cadaver-exquisito-angular.firebaseapp.com",
+    databaseURL: "https://cadaver-exquisito-angular.firebaseio.com",
+    projectId: "cadaver-exquisito-angular",
+    storageBucket: "cadaver-exquisito-angular.appspot.com",
+    messagingSenderId: "169076116527"
+  }
 };
